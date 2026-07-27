3e1a0795f24b29fcd577703fc112b83ea5d7a9f6
Arjela Bezati <arielabezatidev1@gmail.com>
2026-07-27 19:51:24 +0200

    Upate login API URL

--- login.js
+++ login.js
@@ -21,28 +21,19 @@ loginForm.addEventListener("submit", async (e)=>{
     try{
 
 
-        const response = await fetch(
-            "http://localhost:5000/api/auth/login",
-            {
-
-                method:"POST",
-
-                headers:{
-
-                    "Content-Type":"application/json"
-
-                },
-
-
-                body:JSON.stringify({
-
-                    email,
-                    password
-
-                })
-
-            }
-        );
+    const response = await fetch(
+    "https://booksphere-m7ex.onrender.com/api/auth/login",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    }
+);
 
 
 
