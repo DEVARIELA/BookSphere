7b69968777c7b36989ef42a7b8f532640b646abb
DEVARIELA <arielabezatidev1@gmail.com>
2026-07-27 19:45:29 +0200

    Change registration API endpoint to production URL
    Updated the API endpoint for user registration to a new URL.

--- register.js
+++ register.js
@@ -17,26 +17,18 @@ registerForm.addEventListener("submit", async (e) => {
 
 
         const response = await fetch(
-            "http://localhost:5000/api/auth/register",
-            {
-
-                method: "POST",
-
-                headers: {
-                    "Content-Type": "application/json"
-                },
-
-
-                body: JSON.stringify({
-
-                    name,
-                    email,
-                    password
-
-                })
-
-            }
-        );
+    "https://booksphere-m7ex.onrender.com/api/auth/login",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    }
+);
 
 
 
